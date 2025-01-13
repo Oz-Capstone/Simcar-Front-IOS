0613c6cb966aad91970af77032a1472d489d1af3
3DUCK <kermit7520k@gmail.com>
2025-01-13 13:05:44 +0900

    Initial Commit

---
+++ SimCar/SimCarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimCarApp.swift
+//  SimCar
+//
+//  Created by 김건우 on 2025/01/13.
+//
+
+import SwiftUI
+
+@main
+struct SimCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
